05953acf08c2a4a0267f472b03642d56a8d1ad56
mathmorato <morato@ufg.br>
2026-09-07 10:28:48 -0300

    v.1.1.2: adiciona otimizações e responsividade completa para telas mobile em smartphones

--- js/extractor.js
+++ js/extractor.js
@@ -1,6 +1,6 @@
 /* ==========================================================================
    LISTA DE PRESENTES - RHEBECA & MATHEUS
-   Versão: v.1.1.1
+   Versão: v.1.1.2
    Módulo: Extrator Inteligente Resiliente com Heurística de URLs e Metadados
    ========================================================================== */
 
